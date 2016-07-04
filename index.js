9daea42fef8564c6902cd8ec9ff6de489c1cbf1f
Dmitri Akatov <akatov@gmail.com>
2016-07-04 21:36:52 +0100

    export node_modules's bowser object as 'ember-bowser'

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+
 module.exports = {
-  name: 'ember-bowser'
+
+  name: 'ember-bowser',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    var vendor = this.treePaths.vendor;
+    app.import(vendor + '/bowser/bowser.js', {
+      exports: {
+        'bowser': ['default']
+      }
+    });
+  },
+
+  treeForVendor: function(vendorTree) {
+    var trees = [];
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+    var nodeModules = path.join(__dirname, 'node_modules');
+    var bowserPath = path.dirname(require.resolve('bowser'));
+    trees.push(new Funnel(bowserPath, {
+      destDir: 'bowser',
+      include: ['bowser.js'],
+    }));
+    return mergeTrees(trees);
+  }
 };
